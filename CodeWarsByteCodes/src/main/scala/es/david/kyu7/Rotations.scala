33f9cc60760551666845b47afa818564831ec17c
David López <davidlghellin@gmail.com>
2018-08-11 00:11:54 +0200

    estructura de rotations, no entiendo problema

---
+++ CodeWarsByteCodes/src/main/scala/es/david/kyu7/Rotations.scala
@@ -0,0 +1,8 @@
+package es.david.kyu7
+
+object Rotations {
+  //https://www.codewars.com/kata/all-inclusive/train/scala
+  def containAllRots(strng: String, arr: List[String]): java.lang.Boolean = {
+    ???
+  }
+}
