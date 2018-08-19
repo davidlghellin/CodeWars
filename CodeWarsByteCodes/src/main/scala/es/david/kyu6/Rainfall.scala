eb9e1e3aba23bb0a343899415c541ede4f5f20ec
David López <davidlghellin@gmail.com>
2018-08-20 01:00:04 +0200

    LLuvia (sd,mean) pasa test locales, no test ocultos codewars V2

--- CodeWarsByteCodes/src/main/scala/es/david/kyu6/Rainfall.scala
+++ CodeWarsByteCodes/src/main/scala/es/david/kyu6/Rainfall.scala
@@ -4,29 +4,45 @@ object Rainfall {
 
   def mean(town: String, strng: String): Double = {
     val arrayDatos: Array[Float] = getDatos(strng, town)
-    arrayDatos.sum / arrayDatos.length
+    val len = arrayDatos.length
+    len match {
+      case a if a < 12 => -1.0
+      case _ => arrayDatos.sum / arrayDatos.length
+    }
+
   }
 
   def variance(town: String, strng: String): Double = {
 
     val arrayDatos: Array[Float] = getDatos(strng, town)
+    val len = arrayDatos.length
+    len match {
+      case a if a < 12 => -1.0
+      case _ => {
+        val avg = arrayDatos.sum / arrayDatos.length
 
-    val avg = arrayDatos.sum / arrayDatos.length
+        arrayDatos.map(_.toDouble).map(a => math.pow(a - avg, 2)).sum / arrayDatos.size
+      }
+    }
 
-    arrayDatos.map(_.toDouble).map(a => math.pow(a - avg, 2)).sum / arrayDatos.size
   }
 
-  def getDatos(listCiudades: String, ciudad: String) = {
+  def getDatos(listCiudades: String, ciudad: String): Array[Float] = {
     val lista: Array[String] = listCiudades.split("\n")
     lista.foreach(println)
 
     val datosCiudad = lista.filter(x => x.startsWith(ciudad))(0)
     println("----------")
 
-    val mesData: String = datosCiudad.split(":")(1)
-    val arrayMesData: Array[String] = mesData.split(",")
-    val arrayDatos: Array[Float] = arrayMesData.map(x => x.split(" ")(1).toFloat)
-    arrayDatos
+    datosCiudad.split(":").length match {
+      case 1 => Array(-1.0f)
+      case _ => {
+        val mesData: String = datosCiudad.split(":")(1)
+        val arrayMesData: Array[String] = mesData.split(",")
+        val arrayDatos: Array[Float] = arrayMesData.map(x => x.split(" ")(1).toFloat)
+        arrayDatos
+      }
+    }
   }
 
 }
