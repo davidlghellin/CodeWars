c212d6181d91013f1f94755355f928ad8efd221a
David López <davidlghellin@gmail.com>
2018-08-20 19:38:36 +0200

    Productorio, todos menos el actual

--- CodewarsPython/src/main/es/david/kyu7/array_series/ProductArray.py
+++ CodewarsPython/src/main/es/david/kyu7/array_series/ProductArray.py
@@ -1,20 +1,33 @@
 # https://www.codewars.com/kata/product-array-array-series-number-5/train/python
 
 def product_array(numbers):
-    #your code here
+
     lista = []
-    # for iidx, val in numbers:
-    #     aux = 1
-    #     for jidx in numbers:
-    #         if iidx != jidx:
-    #             aux = aux * numbers[jidx]
-    #     lista.append(aux)
-
-
-    # for elem in numbers:
-    #     lista_aux = list(numbers)
-    #     lista_aux = lista_aux.remove(elem)
-    #
-    #     lista.append()
+    for iidx, val in enumerate(numbers):
+        salida = mult(numbers,iidx)
+        lista.append(salida)
 
     return lista
+
+
+def mult(v, pos):
+    salida = 1
+    for i, valor in enumerate(v):
+        if i != pos:
+            salida = salida * valor
+    return salida
+########################################
+
+from operator import mul
+from functools import reduce
+
+def product_array2(numbers):
+    tot = reduce(mul,numbers)
+    return [tot//n for n in numbers]
+
+########################################
+#from numpy import prod
+
+#def product_array3(numbers):
+#    p = prod(numbers)
+#    return [p // i for i in numbers]
